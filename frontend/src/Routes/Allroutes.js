46b1d728cc80cef4476b826e17472f28a8ed7fd3
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-03-24 22:15:27 +0530

    frontend basic structure

---
+++ frontend/src/Routes/Allroutes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import Login from '../Pages/Login'
+import Register from '../Pages/Register'
+
+function Allroutes() {
+  return (
+    <div>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+
+        </Routes>
+    </div>
+  )
+}
+
+export default Allroutes
